c121e7153f00b207f8e63f20e85663258178dda6
mvbertos <matheus.bertos2@gmail.com>
2025-12-09 17:17:06 -0300

    re introduced value format

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//create function for sum,subtraction, division and multiply operetion
-//each operation will require three variables, n1,operator and n2
-//each one will have a variable to be stored
-//when pressed '=' the operate function will be called
-//it will use one of the implemented operation and display the result later
-//the user input will be displayed at the display
 let storedVal = "";
 let currentVal = "";
 let currentOp = null;
@@ -16,7 +10,6 @@ function initCalculator() {
   initOperator();
 }
 
-//INPUTS
 //OperatorPad
 const operatorPad = document.querySelector("#operatorPad");
 function initOperator() {
@@ -43,13 +36,20 @@ equalButton.addEventListener("click", () => {
 function onEqualButtonPressed() {
   if (currentOp) {
     storedVal = operate(storedVal, currentVal, currentOp);
-    display.value = storedVal.toString();
+    display.value = formatNumber(storedVal);
   }
 }
 
 //Clear Values
 const clrButton = document.querySelector("#clrButton");
 clrButton.addEventListener("click", () => clearValues());
+
+function clearValues() {
+  currentOp = null;
+  currentVal = "";
+  storedVal = "";
+  display.value = "";
+}
 
 //Number Pad
 const numberPad = document.querySelector("#numberPad");
@@ -67,48 +67,30 @@ function onNumberPressed(value) {
   display.value = currentVal;
 }
 
-//SETTERS
-/**
- * Clears all values inputted by the user
- * this include the display value.
- */
-function clearValues() {
-  currentOp = null;
-  currentVal = "";
-  storedVal = "";
-  display.value = "";
-}
-
-//OPERATION
-/**
- * operate will receives any parameters because it uses the n1val, opval and n2val
- * this is a fixed rule to not get lost between variables when the operation is being done
- * "yeah I did got lost once"
- * @returns
- */
+//Operate
 function operate(a, b, op) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   let result = "";
-  let n1 = parseInt(a);
-  let n2 = parseInt(b);
-  console.log(n1 + op + n2);
+  console.log(a + op + b);
 
   switch (op) {
     case "+":
-      result = add(n1, n2);
+      result = add(a, b);
       break;
     case "-":
-      result = substract(n1, n2);
+      result = substract(a, b);
       break;
     case "÷":
-      if (n1 == 0 || n2 == 0) {
+      if (a == 0 || b == 0) {
         alert("0 values cannot be divided in this simple calculator.");
         clearValues();
         return result;
       }
-      result = divide(n1, n2);
+      result = divide(a, b);
       break;
     case "x":
-      result = multiply(n1, n2);
+      result = multiply(a, b);
       break;
     default:
       alert("Operator is not valid or missing.");
@@ -132,40 +114,19 @@ function divide(a, b) {
   return a / b;
 }
 
-//FORMAT AND VALIDATE
 /**
  * it will get the number on a string and fix to two decimals or a full integer
  * @param {*} value - preferable string
  * @returns
  */
 function formatNumber(value = "") {
-  // if (Number.isInteger(value)) {
-  //   return value;
-  // } else {
-  //   return value.toFixed(2).replace(/\.?0$/, "");
-  // }
+  if (!isNaN(parseFloat(value))) {
+    let round = Math.round(value * 100) / 100;
+    return round.toString().replace(/\.?0+$/, "");
+  }
+  console.error("tried to format a non number value " + value);
   return value;
 }
-
-// /**
-//  * it will check if parameter is a valid number
-//  * @param {*} val - only string
-//  * @returns
-//  */
-
-// function isValueValid(val) {
-//   console.log("validating:" + val);
-
-//   if (typeof val != "string" || val === "") {
-//     console.error("you tried to validade a non string value or a empty value");
-//     return false;
-//   }
-//   if (isNaN(val) && isNaN(parseFloat(val))) {
-//     console.error(val + " is not a valid number!");
-//     return false;
-//   }
-//   return true;
-// }
 
 //EXECUTE
 initCalculator();
